7eb8c0232caafe16b9118682aa8d62bf18301553
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-03-29 09:23:14 +0530

    Add basics

---
+++ basics/script.js
@@ -0,0 +1,34 @@
+// Create variables
+var firstName = 'Chamara';
+var lastName = 'Perera';
+var age = 26;
+var isOk = true;
+var job, height;
+
+// Print only variable
+console.log(firstName);
+// Print the variable with a string
+console.log('My name is '+lastName);
+// Type Coercion
+console.log('My age is '+age);
+console.log('It is '+isOk);
+console.log('I am a '+job);
+
+// Get variable type
+console.log(typeof(lastName));
+console.log(typeof(age));
+console.log(typeof(isOk));
+console.log(typeof(job));
+
+// Define
+job = 'Doctor';
+// Variable Mutation
+lastName = 'Fernando';
+console.log('I am '+lastName+' I am a '+job);
+
+// Dsiplay an alert
+alert('My name is '+lastName+'. I am '+age+' years old. And am I ok? '+isOk+'.');
+
+// Get user input
+height = prompt('What is your height?');
+alert('My height is '+height+' feet.');

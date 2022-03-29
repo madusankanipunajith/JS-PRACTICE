fb18a8698c26a3316ae0c63c240e1c87202589e6
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-03-29 14:51:17 +0530

    Update switch function using bitwise operators

--- basics/switch.js
+++ basics/switch.js
@@ -29,3 +29,17 @@ switch (true) {
         console.log("Too young");
         break;
 }
+
+
+// Bitwise operators (not recently used but should know) : & | ~ ^ >> <<
+console.log(5 & 1); // AND
+console.log(5 | 1); // OR
+console.log(~5); // NOT
+console.log(5 ^ 1); // XOR 1 1 -> 0 , 0 1 -> 1
+console.log(5 << 1); // Zero fill left shift => 0101 -> 1010
+console.log(5 >> 1); // Signed right shift => 0101 -> 0010
+console.log(5 >>> 1); // Zero fill right shift => 0101 -> 0010
+
+console.log((10).toString(2)); // 10 to binary
+console.log((0b1010).toString(10)); // binary to decimal
+console.log(parseInt("1010", 2));

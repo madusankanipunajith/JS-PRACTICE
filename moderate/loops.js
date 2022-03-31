9cb3617a337d135dda1cfad28bb8a8f3400b6378
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-03-31 18:53:01 +0530

    Add loops part one

---
+++ moderate/loops.js
@@ -0,0 +1,14 @@
+for (let index = 0; index <= 10; index++) {
+    console.log("1 : "+index);
+}
+
+for (let index = 10; index >= 0; index--){
+    console.log("2 : "+index);
+}
+
+var students = ["Nimal", "Kamal", "Dasun", "John"];
+for (let index = 0; index < students.length; index++) {
+    const element = students[index];
+    console.log(element);
+}
+

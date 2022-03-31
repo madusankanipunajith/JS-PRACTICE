b707282ec411fb687a3322a26392cab1cec1f702
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-03-31 19:09:18 +0530

    Add loops part two and complete the loop section

--- moderate/loops.js
+++ moderate/loops.js
@@ -1,3 +1,4 @@
+// for loops
 for (let index = 0; index <= 10; index++) {
     console.log("1 : "+index);
 }
@@ -11,4 +12,42 @@ for (let index = 0; index < students.length; index++) {
     const element = students[index];
     console.log(element);
 }
+
+// for/in loops => Loops through the properties of the object
+var person ={
+    firstName: "Chamara",
+    lastName: "Silva",
+    age: 25
+};
+
+for (const i in person) {
+    if (Object.hasOwnProperty.call(person, i)) {
+        const element = person[i];
+        console.log(element);
+    }
+}
+
+// for/of loops => Loops over the data structures such as arrays, strings and etc
+var fullName = "Madusanka Nipunajith";
+for (const i of fullName) {
+    console.log(i);
+}
+for (const iterator of students) {
+    console.log(iterator);
+}
+
+// while loops
+var marks = [33,44,55,66,77,88,99,100];
+let k = 0;
+while (k < marks.length) {
+    console.log(marks[k]);
+    k++;
+}
+
+// do-while loops
+let l = 10;
+do {
+    console.log(l);
+    l--;
+} while (l>=1);
 

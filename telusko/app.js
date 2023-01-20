91c3ec30045bd1377fc5e1385383ab72e3d52054
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2023-01-20 07:51:41 +0530

    Add routes

--- telusko/app.js
+++ telusko/app.js
@@ -1,7 +1,22 @@
 const calc = require('./calc');
 const express = require('express');
+const app = express();
 
-let result = calc.add(4,5);
+app.get('/', (req, res)=>{
+    console.log('Hello World');
+    res.status(200).send('Hello World');
+})
 
-console.log("Output is "+ result);
+app.get('/alian', (req, res)=>{
+    var id = req.query.id
+    res.status(200).send('Welcome back Alian ' + id);
+})
 
+app.get('/alian/:id', (req, res)=>{
+    var id = req.params.id
+    res.status(200).send('Hi Madusanka ' + id);
+})
+
+app.listen(9000, (req, res)=>{
+    console.log('Running on port 9000');
+});
